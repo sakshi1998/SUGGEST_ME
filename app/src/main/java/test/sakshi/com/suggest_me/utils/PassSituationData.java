f72559206d4b0a7e000ab36b5fbc93af48b889df
sakshi1998 <sharmasakshi19aug@gmail.com>
2018-01-04 23:56:30 +0530

    Update PassSituationData.java

--- app/src/main/java/test/sakshi/com/suggest_me/utils/PassSituationData.java
+++ app/src/main/java/test/sakshi/com/suggest_me/utils/PassSituationData.java
@@ -1,8 +1,6 @@
 package test.sakshi.com.suggest_me.utils;
 
-/**
- * Created by Divya on 11/29/2016.
- */
+
 public interface PassSituationData {
     public void onDataPass(Integer id, String nameText);
 }
